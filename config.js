7946aa819196b28d1ed78bf11923802f4531ffc7
Christopher <70889338+hwk-c@users.noreply.github.com>
2022-11-23 23:34:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        turn_on: "lets start!",
+        play: "Music~!",
+        bannar_coming: "Colour~",
+        balloons_flying: "Seems like something is still missing...",
+        cake_fadein: "Cake???",
+        light_candle: "Candle???",
+        wish_message: "Happy Birthday to my Bee Bee :D",
         story: "A MESSAGE FOR YOU",
     }
 };
